e349b559f3f00d5e6ccf4def29016f212f865e43
sachin1kumar <sachinkmr375@gmail.com>
2018-11-08 19:48:43 +0530

    final version 1.0 release.

--- app/src/main/java/location/detailed/com/mydetailedlocation/MapsActivity.kt
+++ app/src/main/java/location/detailed/com/mydetailedlocation/MapsActivity.kt
@@ -10,7 +10,6 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
